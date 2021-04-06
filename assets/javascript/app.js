31d752e30348adb9bdfba49849848e4bbb863555
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-06 22:51:04 +0200

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+
+//RICHIESTA NOME UTENTE
+var firstName = prompt("Come ti chiami ?");
+
+//RICHIESTA COGNOME UTENTE
+var surname = prompt("Qual è il tuo cognome ?");
+
+//RICHIESTA COLORE PREFERITO
+var color = prompt("Qual è il tuo colore preferito ?")
+
+//
+document.getElementById("password").innerHTML = firstName + surname + color + 21;
+
